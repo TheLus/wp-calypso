05a04dc9f9075340fd05619b721ff48184227cc1
Omar Jackman <github@omarjackman.com>
2016-08-23 14:32:29 -0400

    Add mock course list data and display it on the courses page

--- client/me/help/help-courses/index.jsx
+++ client/me/help/help-courses/index.jsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { connect } from 'react-redux';
-import { localize } from 'i18n-calypso';
+import i18n, { localize } from 'i18n-calypso';
 import { find } from 'lodash';
 
 /**
@@ -16,16 +16,52 @@ import {
 import { getCurrentUserId } from 'state/current-user/selectors';
 import { PLAN_BUSINESS } from 'lib/plans/constants';
 
+const courses = [
+	{
+		title: i18n.translate( 'How to Make a Business Site on WordPress.com' ),
+		description: i18n.translate(
+			'Bacon ipsum dolor amet jowl swine t-bone capicola porchetta andouille' +
+			'pork chop tail chicken. Ball tip frankfurter ham pastrami pork chop shank pork loin, bacon prosciutto. ' +
+			'Turducken pig meatloaf short ribs kielbasa. Chicken swine spare ribs ball tip pork loin cow drumstick ' +
+			'tail tongue venison tenderloin short ribs kielbasa salami. Andouille boudin prosciutto jowl tongue, bacon ' +
+			'chuck tail flank ball tip strip steak leberkas filet mignon. Tri-tip pancetta prosciutto spare ribs ball tip.'
+		),
+		schedule: [
+			{
+				date: i18n.moment( new Date( '8/25/2016 1:00 pm PST' ) ),
+				registrationUrl: 'http://wordpress.com'
+			},
+			{
+				date: i18n.moment( new Date( '8/27/2016 9:00 am PST' ) ),
+				registrationUrl: 'http://wordpress.com'
+			},
+			{
+				date: i18n.moment( new Date( '8/31/2016 10:30 am PST' ) ),
+				registrationUrl: 'http://wordpress.com'
+			},
+		],
+		recent: [
+			{
+				date: i18n.moment( new Date( '8/1/2016 10:30 am PST' ) ),
+				video: 'http://wordpress.com/some-video.mp4'
+			}
+		]
+	}
+];
+
 function mapStateToProps( state ) {
 	const userId = getCurrentUserId( state );
 	const purchases = getUserPurchases( state, userId );
-	const isBusinessPlanUser = purchases && !! find( purchases, purchase => purchase.productSlug === PLAN_BUSINESS );
+	const showRecentCourseRecordings = purchases && !! find( purchases, purchase => purchase.productSlug === PLAN_BUSINESS );
 	const isLoading = ! hasLoadedUserPurchasesFromServer( state );
+
+	//TODO: Add tracks pings
 
 	return {
 		isLoading,
-		isBusinessPlanUser,
-		userId
+		showRecentCourseRecordings,
+		userId,
+		courses
 	};
 }
 
